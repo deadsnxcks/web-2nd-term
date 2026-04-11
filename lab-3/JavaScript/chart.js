4b71e739786683dc7bc8fbcd0ba2cd608c237c1d
deadsnxcks <ignat.kkovalev@gmail.com>
2026-04-11 21:47:15 +1000

    feat(lab-3): part 3/3 complete

--- lab-3/JavaScript/chart.js
+++ lab-3/JavaScript/chart.js
@@ -15,14 +15,11 @@ function createArrGraph(data, key) {
     return arrGraph;
 }
 
-// Добавили параметр keyX (по умолчанию можно оставить 'Страна', если ничего не передали)
-function drawGraph(data, keyX = 'Страна', mode = 'both') {
-    // создаем массив для построения графика
+function drawGraph(data, keyX = 'Страна', mode = 'both', chartType = 'scatter') {
+    
     let arrGraph = createArrGraph(data, keyX);
 
-    // Умная сортировка в зависимости от того, что мы выводим по оси X
     if (keyX === 'Год') {
-        // Сортируем года по возрастанию (как числа)
         arrGraph.sort((a, b) => a.labelX - b.labelX);
     }
 
@@ -34,13 +31,15 @@ function drawGraph(data, keyX = 'Страна', mode = 'both') {
         height: parseFloat(svg.style('height')),
         marginX: 50,
         marginY: 50
-    }
+    };
        
-    // создаем шкалы преобразования и выводим оси
-    const [scX, scY] = createAxis(svg, arrGraph, attr_area, mode);
-
-    // рисуем график
-    createChart(svg, arrGraph, scX, scY, attr_area, mode);        
+    if (chartType === 'scatter') {
+        const [scX, scY] = createAxis(svg, arrGraph, attr_area, mode);
+        createChart(svg, arrGraph, scX, scY, attr_area, mode);        
+    } else if (chartType === 'bar') {
+        const [scX, scY, scX1] = createAxisBar(svg, arrGraph, attr_area, mode);
+        createChartBar(svg, arrGraph, scX, scY, scX1, attr_area, mode);
+    }
 }
 
 function createAxis(svg, data, attr_area, mode){
@@ -100,4 +99,73 @@ function createChart(svg, data, scaleX, scaleY, attr_area, mode) {
         .attr("cy", d => scaleY(d.y))
         .attr("transform", `translate(${attr_area.marginX}, ${attr_area.marginY})`)
         .style("fill", d => d.color)
+}
+
+function createAxisBar(svg, data, attr_area, mode) {
+    // Ось Y (такая же, как в точечной)
+    let allVals = [];
+    if (mode === 'max') allVals = data.map(d => d.values[1]);
+    else if (mode === 'min') allVals = data.map(d => d.values[0]);
+    else allVals = data.flatMap(d => [d.values[0], d.values[1]]);
+
+    const [minV, maxV] = d3.extent(allVals);
+
+    const scaleY = d3.scaleLinear()
+        .domain([minV * 0.9, maxV * 1.1])
+        .range([attr_area.height - 2 * attr_area.marginY, 0]);               
+
+    // Ось X (добавлен padding для отступов между группами)
+    const scaleX = d3.scaleBand()
+        .domain(data.map(d => d.labelX))
+        .range([0, attr_area.width - 2 * attr_area.marginX])
+        .padding(0.2); 
+                    
+    // Вложенная ось X1 (для разделения синего и красного столбца)
+    let innerDomain = [];
+    if (mode === 'min' || mode === 'both') innerDomain.push('min');
+    if (mode === 'max' || mode === 'both') innerDomain.push('max');
+
+    const scaleX1 = d3.scaleBand()
+        .domain(innerDomain)
+        .range([0, scaleX.bandwidth()]);
+
+    const axisX = d3.axisBottom(scaleX);
+    const axisY = d3.axisLeft(scaleY);
+
+    svg.append("g")
+        .attr("transform", `translate(${attr_area.marginX}, ${attr_area.height - attr_area.marginY})`)
+        .call(axisX)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-45)");
+    
+    svg.append("g")
+        .attr("transform", `translate(${attr_area.marginX}, ${attr_area.marginY})`)
+        .call(axisY);
+
+    return [scaleX, scaleY, scaleX1];
+}
+
+function createChartBar(svg, data, scaleX, scaleY, scaleX1, attr_area, mode) {
+    const rects = [];
+    data.forEach(d => {
+        if (mode === 'min' || mode === 'both') 
+            rects.push({x: d.labelX, y: d.values[0], type: "min", color: "blue"});
+        if (mode === 'max' || mode === 'both') 
+            rects.push({x: d.labelX, y: d.values[1], type: "max", color: "red"});
+    });
+
+    svg.selectAll(".bar")
+        .data(rects)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", d => scaleX(d.x) + scaleX1(d.type)) // Сдвигаем столбец внутри группы
+        .attr("y", d => scaleY(d.y))
+        .attr("width", scaleX1.bandwidth()) // Ширина одного столбца
+        .attr("height", d => (attr_area.height - 2 * attr_area.marginY) - scaleY(d.y))
+        .attr("transform", `translate(${attr_area.marginX}, ${attr_area.marginY})`)
+        .style("fill", d => d.color);
 }
--- lab-3/JavaScript/main.js
+++ lab-3/JavaScript/main.js
@@ -13,5 +13,35 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    drawGraph(buildings, 'Год', 'max');
+    const buildGraphBtn = d3.select("#buildGraphBtn");
+    const errorSpan = d3.select("#yAxisError");
+
+    buildGraphBtn.on("click", function() {
+
+        const xAxisOption = d3.select('input[name="xAxis"]:checked').property("value");
+
+        const isMaxChecked = d3.select("#oyMax").property("checked");
+        const isMinChecked = d3.select("#oyMin").property("checked");
+
+        if (!isMaxChecked && !isMinChecked) {
+            errorSpan.style("display", "inline");
+            return;
+        } else {
+            errorSpan.style("display", "none"); 
+        }
+
+        let mode = "";
+        if (isMaxChecked && isMinChecked) {
+            mode = "both";
+        } else if (isMaxChecked) {
+            mode = "max";
+        } else if (isMinChecked) {
+            mode = "min";
+        }
+
+        const chartType = d3.select("#chartType").property("value");
+
+        drawGraph(buildings, xAxisOption, mode, chartType);
+    });
+
 });
